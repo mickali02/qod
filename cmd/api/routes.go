175a88bc8745b87f38f1ea7c0767a107af2a8513
Mickali Garbutt <2020151994@ub.edu.bz>
2025-08-26 10:46:44 -0600

    Encoding JSON v7

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -8,9 +8,16 @@ import (
 )
 
 // routes specifies our routes
-func (app *application) routes() http.Handler {
-	router := httprouter.New()
-	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+func (a *application)routes() http.Handler  {
 
-	return router
+   // setup a new router
+   router := httprouter.New()
+   // handle 404
+   router.NotFound = http.HandlerFunc(a.notFoundResponse)
+  // handle 405
+   router.MethodNotAllowed = http.HandlerFunc(a.methodNotAllowedResponse)
+   // setup routes
+   router.HandlerFunc(http.MethodGet, "/v1/healthcheck", a.healthcheckHandler)
+   return router
+  
 }
